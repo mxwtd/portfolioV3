12fd3d55c0e1dea6824ca68ba21ad38fa5b73523
max whitehead <fifes-catkin.0r@icloud.com>
2023-03-25 17:20:51 +0300

    Safety commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./stylesheets/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
