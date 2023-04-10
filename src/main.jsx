26b565ceeef71424b01962216de0e89835fb4ca8
max whitehead <fifes-catkin.0r@icloud.com>
2023-04-10 08:11:56 +0300

    Prepare for GitHub Pages deployment 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <HashRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HashRouter>,
 );
